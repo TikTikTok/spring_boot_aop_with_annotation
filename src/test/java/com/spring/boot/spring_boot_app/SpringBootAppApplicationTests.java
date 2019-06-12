d76f620e4af5b37f526a64fb6bce447d7473785c
TikTikTok <chandan.singh116@gmail.com>
2019-06-12 16:06:20 +0530

    added @ContextConfiguration annotation for spring runner test class

--- src/test/java/com/spring/boot/spring_boot_app/SpringBootAppApplicationTests.java
+++ src/test/java/com/spring/boot/spring_boot_app/SpringBootAppApplicationTests.java
@@ -3,10 +3,12 @@ package com.spring.boot.spring_boot_app;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest(classes = "")
+@ContextConfiguration
 public class SpringBootAppApplicationTests {
 
 	@Test
